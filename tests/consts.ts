bd1869987c9a620df7e5c5f823b01a4e00f6af14
AzrielMM <78009026+AzrielMM@users.noreply.github.com>
2022-04-13 16:17:30 -0700

    restructure

--- tests/consts.ts
+++ tests/consts.ts
@@ -1,4 +1,4 @@
-import "./ArrayNumberExtends";
+import "../scripts/ArrayNumberExtends";
 
 const num_list: Array<number> = [1, 2, 45, 11, 122, 4, 5, 3, 41, 1, 22, 3, 4, 5, 6, 7, 8, 9, 10, 122.1, 11.11111111, 1.7, 1.5];
 const string_list: Array<string> = ["hello", "world", "this", "is", "a", "test"];
