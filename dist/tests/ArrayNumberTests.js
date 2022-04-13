915c95f2a46a1af4e8c4df0f5d60bc69dea580b6
AzrielMM <78009026+AzrielMM@users.noreply.github.com>
2022-04-13 16:29:36 -0700

    recompiled

--- dist/tests/ArrayNumberTests.js
+++ dist/tests/ArrayNumberTests.js
@@ -2,6 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 require("../scripts/ArrayNumberExtends");
 const num_list = [1, 2, 45, 11, 122, 4, 5, 3, 41, 1, 22, 3, 4, 5, 6, 7, 8, 9, 10, 122.1, 11.11111111, 1.7, 1.5];
-const string_list = ["hello", "world", "this", "is", "a", "test"];
 console.log(num_list.toIntArray(), num_list.max(), num_list.min());
 //# sourceMappingURL=ArrayNumberTests.js.map
